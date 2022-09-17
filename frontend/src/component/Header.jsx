af1b78d0330aefa84f87a1c509ecabb51de56739
karasakalmt <karasakal99@gmail.com>
2022-09-17 20:36:55 +0200

    wallet

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,10 +1,13 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 const Header = () => {
   return (
-    <div className="block p-6 m-3 border-4 border-white border-dashed">
+    <div className="block p-6 m-3 border-4 border-white border-dashed flex justify-between">
       <div className="flex">
         <img src='/logo.png' alt="Hedwig" style={{width:'3em'}}/>
         <p className="ml-2 text-3xl font-bold	">HEDWIG</p>
       </div>
+      <ConnectButton />
     </div>
   )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [chain.polygon],
+  [
+    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Hedwig',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>
 );
 
