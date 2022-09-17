7eaedb830f6c2518c8f62a1daccefd9eb28f9413
İzzet Emre Demir <izzetemredemir@gmail.com>
2022-09-17 23:35:15 +0200

    lens search profile added

--- frontend/src/lensApi.js
+++ frontend/src/lensApi.js
@@ -203,3 +203,61 @@ export const getProfile = `query Profile($request: SingleProfileQueryRequest!) {
     }
   }
 `
+export const searchProfiles = `
+  query Search($query: Search!, $type: SearchRequestTypes!) {
+    search(request: {
+      query: $query,
+      type: $type,
+      limit: 10
+    }) {
+      ... on ProfileSearchResult {
+        __typename 
+        items {
+          ... on Profile {
+            ...ProfileFields
+          }
+        }
+        pageInfo {
+          prev
+          totalCount
+          next
+        }
+      }
+    }
+  }
+  fragment MediaFields on Media {
+    url
+  }
+  fragment ProfileFields on Profile {
+    profileId: id,
+    name
+    bio
+    ownedBy
+    attributes {
+      displayType
+      traitType
+      key
+      value
+    }
+    metadata
+    isDefault
+    handle
+    picture {
+      ... on NftImage {
+        contractAddress
+        tokenId
+        uri
+        verified
+      }
+      ... on MediaSet {
+        original {
+          ...MediaFields
+        }
+      }
+    }
+    stats {
+      totalFollowers
+      totalFollowing
+    }
+  }
+`
