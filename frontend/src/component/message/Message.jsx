088ff1b82bcd1723a268b3de5138e55abb576ba9
karasakalmt <karasakal99@gmail.com>
2022-09-17 23:04:14 +0200

    messaging

--- frontend/src/component/message/Message.jsx
+++ frontend/src/component/message/Message.jsx
@@ -1,6 +1,10 @@
-const Message = () => {
+const Message = ({ isClient, message }) => {
   return (
-    <div>Message</div>
+    <div className={isClient ?"w-full flex justify-end": "w-full flex"}>
+        <div className={"bg-neutral-900 w-fit px-4 py-3 mt-2"}>
+            {message}
+        </div>
+    </div>
   )
 }
 
--- frontend/src/component/message/Messages.jsx
+++ frontend/src/component/message/Messages.jsx
@@ -1,8 +1,51 @@
+import Message from "./Message"
 import MessageField from "./MessageField"
+
+const messages = [
+    {
+        message:'ETHBerlin is awesome!',
+        isClient:false
+    },
+    {
+        message:'True!',
+        isClient:true
+    },
+    {
+        message:'ETHBerlin is awesome!',
+        isClient:false
+    },
+    {
+        message:'ETHBerlin is awesome!',
+        isClient:false
+    },
+    {
+        message:'ETHBerlin is awesome!',
+        isClient:true
+    },
+    {
+        message:'ETHBerlin is awesome!',
+        isClient:false
+    },
+    {
+        message:'ETHBerlin is awesome!',
+        isClient:false
+    },
+    {
+        message:'ETHBerlin is awesome!',
+        isClient:true
+    },
+    {
+        message:'ETHBerlin is awesome!',
+        isClient:false
+    },
+]
 
 const Messages = () => {
   return (
     <div className="col-span-6 p-2 relative">
+        {messages.map((message) => {
+            return <Message isClient={message.isClient} message={message.message}/>
+        })}
         <MessageField/>
     </div>
   )
