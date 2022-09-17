19278e674e97bc0da52da40e3e0231b2df2e98ce
karasakalmt <karasakal99@gmail.com>
2022-09-17 14:14:37 +0200

    header contacts and messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,13 @@
+import Contacts from "./component/contact/Contacts";
+import Header from "./component/Header";
+import Messages from "./component/message/Messages";
+
 function App() {
   return (
     <div>
+      <Header/>
+      <Contacts/>
+      <Messages/>
     </div>
   );
 }
---
+++ frontend/src/component/contact/Contacts.jsx
@@ -0,0 +1,7 @@
+const Contacts = () => {
+  return (
+    <div>Contacts</div>
+  )
+}
+
+export default Contacts
