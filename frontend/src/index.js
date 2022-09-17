1fa0ab1f20063c647ffebc0f31fcd5f7f2b7e435
karasakalmt <karasakal99@gmail.com>
2022-09-18 00:18:49 +0200

    rainbowkit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import '@rainbow-me/rainbowkit/styles.css';
 
 import {
   getDefaultWallets,
+  midnightTheme,
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
 import {
@@ -40,13 +41,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider chains={chains} theme={midnightTheme({accentColor:'white', accentColorForeground:'#000000', borderRadius:'none'})} coolMode>
         <App />
       </RainbowKitProvider>
     </WagmiConfig>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
