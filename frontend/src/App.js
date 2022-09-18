e04d840e9869e5ac5f11a058cbc2af7abd051483
karasakalmt <karasakal99@gmail.com>
2022-09-18 04:28:22 +0200

    messages userchange

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import Contacts from "./component/contact/Contacts";
 import Header from "./component/Header";
 import Messages from "./component/message/Messages";
+import {useState} from 'react';
+
 function App() {
+  const [contact, setContact] = useState('');
+  
+  const handleChange = async (contact) => {
+    setContact(contact);
+  }
+
   return (
     <div>
       <Header/>
       <div className="grid grid-cols-8 m-3 border-4 border-white border-dashed" style={{ height:'85vh' }}>
-        <Contacts/>
-        <Messages/>
+        <Contacts handleUserChange={handleChange}/>
+        <Messages currentContact={contact}/>
       </div>
     </div>
   );
